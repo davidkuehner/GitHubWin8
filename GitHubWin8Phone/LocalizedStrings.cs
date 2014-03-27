1a0fe5a8a0fee8417a4e8ce7d0b52cdec8f39eb0
David Kühner <dav.kuhn@gmail.com>
2014-03-27 07:38:26 -0700

    Initial code commit

---
+++ GitHubWin8Phone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using GitHubWin8Phone.Resources;
+
+namespace GitHubWin8Phone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
