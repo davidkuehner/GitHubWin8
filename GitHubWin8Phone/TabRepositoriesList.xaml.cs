ac1f3aa3726fb6fa79d6b398c92c27a5a10e83c5
damiano.renfer <damiano.renfer@he-arc.ch>
2014-04-03 16:45:10 +0200

    Added News and repo tabs (empty)

---
+++ GitHubWin8Phone/TabRepositoriesList.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace GitHubWin8Phone
+{
+    public partial class TabRepositoriesList : UserControl
+    {
+        public TabRepositoriesList()
+        {
+            InitializeComponent();
+        }
+    }
+}
